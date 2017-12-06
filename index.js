0d71261eb1332b87297f916e98f3258000766ee1
capucinecoudert <coudert.capucine@gmail.com>
2017-12-06 18:37:17 +0100

    fix exclamation mark

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ http.createServer(function (req, res) {
 // sending a response header of 200 OK 
   res.writeHead(200, {'Content-Type': 'text/plain'}); 
   // print out Hello World 
-  res.end('Hello World :-)\n'); 
+  res.end('Hello World !\n'); 
   // use port 8080 
 }).listen(8080); 
 
